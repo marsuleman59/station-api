dd8baed4b6ad974d38cfc0c38ce3d32d5953fb81
Suleman <marsuleman@hotmail.com>
2018-09-28 09:31:12 -0500

    Initial commit

---
+++ src/test/java/com/marsuleman59/stationapi/StationApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marsuleman59.stationapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StationApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
